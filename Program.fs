1787c450fa81ab8bcfe500ef93b472dea8f46f59
mike stedman <github@ravuya.com>
2016-10-11 15:48:31 -0600

    Simple demo with jobs based on https://neoeinstein.github.io/blog/2016/04-08-hopac-getting-started-with-jobs/index.html

--- Program.fs
+++ Program.fs
@@ -1,16 +1,41 @@
 ﻿open Hopac
 
+// Job demo - run something asynchronously using the job computation expr
+
 let doThingAsynchronously (getThingAsynchronously : Async<_>) (delay : Job<unit>) = job {
     let! result = getThingAsynchronously
     do! delay
     return result
 }
 
-[<EntryPoint>]
-let main argv = 
+let simpleAsyncDemo =
     let myResult = async { return 4 }
     let delay = timeOutMillis 10
     doThingAsynchronously myResult delay
         |> run
         |> printfn "Result was %i"
+
+// Promise demo - a Promise is a "lazy" Job that only gets executed once
+        
+let sideEffect = job {
+    printfn "> Side effect!"
+    return 4
+}
+
+let demoWithoutPromises =
+    printfn "Without memoization"
+    run sideEffect |> printfn "Result 1 was %i"
+    run sideEffect |> printfn "Result 2 was %i" // Side effect was called again here!
+
+let demoWithPromises =
+    printfn "With memoization"
+    let memoized = memo sideEffect
+    run memoized |> printfn "Result was %i"
+    run memoized |> printfn "Result was %i" // Side effect should not be called again here
+
+[<EntryPoint>]
+let main argv = 
+    simpleAsyncDemo
+    demoWithoutPromises
+    demoWithPromises
     0
